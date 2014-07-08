88b65f489a6fa97c77b02c98e485805eac68fc1c
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2014-07-08 22:51:27 +0300

    dependency to infiispan-jcache

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,15 +5,15 @@ mvn clean install -DskipTests
 
 echo "********************************************"
 echo "Removing old deployment files..."
-sudo rm -f $JBOSS_HOME/standalone/deployments/test.ear.*
-sudo rm -f $JBOSS_HOME/standalone/deployments/test.ear
-sudo rm -R -f $JBOSS_HOME/standalone/deployments/test.ear
+rm -f $JBOSS_HOME/standalone/deployments/test.ear.*
+rm -f $JBOSS_HOME/standalone/deployments/test.ear
+rm -R -f $JBOSS_HOME/standalone/deployments/test.ear
 
 echo "********************************************"
 echo "Copying new .ear to deployment folder..."
-sudo cp test-ear/target/test.ear $JBOSS_HOME/standalone/deployments
+cp test-ear/target/test.ear $JBOSS_HOME/standalone/deployments
 
-sudo touch $JBOSS_HOME/standalone/deployments/test.ear.dodeploy
+touch $JBOSS_HOME/standalone/deployments/test.ear.dodeploy
 
 echo "********************************************"
 echo "Application built and deployed successfully!"
