99d42ec9644026d98a89a5e0a2c8bd94851b3357
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2016-07-13 22:25:36 +0300

    Angular 2 web app setup

---
+++ mt-web/src/main/webapp/app/app.component.js
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+export class AppComponent {
+	
+	constructor() {
+	}
+	
+}
+
+AppComponent.annotations = [
+                            	new Component({
+                            		selector: 'mt-app',
+                            		template: '<h1>Angular 2 App</h1>'
+                            	})
+];
