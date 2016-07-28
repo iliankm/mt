1630ad07e295feca0a516e135b7a4e73a71adb11
iliankm <ilian.kamilov@gmail.com>
2016-07-28 10:35:11 +0300

    upgrade Angular 2 packages

--- mt-web/src/main/webapp/systemjs.config.js
+++ mt-web/src/main/webapp/systemjs.config.js
@@ -2,7 +2,7 @@
  * System configuration for Angular 2
  */
 (function(global) {
-	
+
   // map tells the System loader where to look for things
   var map = {
     'app':                        'app', // 'dist',
@@ -11,14 +11,14 @@
     'rxjs':                       'node_modules/rxjs',
     'traceur':                    'node_modules/traceur/bin/traceur.js'
   };
-  
+
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
-  
+
   var ngPackageNames = [
     'common',
     'compiler',
@@ -30,28 +30,29 @@
     'router-deprecated',
     'upgrade',
   ];
-  
+
   // Individual files (~300 requests):
   function packIndex(pkgName) {
     packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
   }
-  
+
   // Bundled (~40 requests):
   function packUmd(pkgName) {
     packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
   }
-  
+
   // Most environments should use UMD; some (Karma) need the individual index files
-  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-  
+  //var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+  var setPackageConfig = packUmd;
+
   // Add package entries for angular packages
   ngPackageNames.forEach(setPackageConfig);
-  
+
   var config = {
     map: map,
     packages: packages
   };
-  
+
   System.config(config);
-  
+
 })(this);
