ca7b72c3124590b0ea9a8955e199cb35c7117fb6
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2016-07-30 10:52:01 +0300

    Routing configuration

--- mt-web/src/main/webapp/app/main.js
+++ mt-web/src/main/webapp/app/main.js
@@ -1,8 +1,13 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {APP_ROUTER_PROVIDERS} from './app.routes';
-
+import {
+	  Location,
+	  LocationStrategy,
+	  HashLocationStrategy
+	} from '@angular/common';
 
 bootstrap(AppComponent, [
-	APP_ROUTER_PROVIDERS
+	APP_ROUTER_PROVIDERS,
+	{provide: LocationStrategy, useClass: HashLocationStrategy}
 ]);
