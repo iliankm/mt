c4b26c082ec5a90df97dfe55bc8673a9796c4618
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2016-08-06 10:10:26 +0300

    Introduce PageNotFound ui component.

---
+++ mt-web/src/main/webapp/app/page-not-found/page-not-found.component.js
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+export class PageNotFoundComponent {
+
+	constructor() {
+	}
+
+}
+
+PageNotFoundComponent.annotations = [
+                            	new Component({
+                            		selector: 'page-not-found',
+                            		templateUrl: 'app/page-not-found/page-not-found.template.html',
+                            		styleUrls:  []
+                            	})
+];
