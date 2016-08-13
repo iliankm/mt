b75c522740d1ac84baf2a9ec65d6da1ad3841d18
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2016-08-13 19:39:44 +0300

    Wizard steps UI component.

--- mt-web/src/main/webapp/app/wizard/wizard-steps/wizard-step.component.js
+++ mt-web/src/main/webapp/app/wizard/wizard-steps/wizard-step.component.js
@@ -3,14 +3,19 @@ import { Component } from '@angular/core';
 export class WizardStepComponent {
 
 	constructor() {
+		this.isCurrent = false;
+		this.isVisited = false;
+		this.name = null;
+		this.title = null;
+		this.info = null;
 	}
-
 }
 
 WizardStepComponent.annotations = [
                             	new Component({
                             		selector: 'wizard-step',
                             		templateUrl: 'app/wizard/wizard-steps/wizard-step.template.html',
-                            		styleUrls:  ['app/wizard/wizard-steps/wizard-step.component.css']
+                            		styleUrls:  ['app/wizard/wizard-steps/wizard-step.component.css'],
+                            		inputs: ['name', 'isVisited', 'title', 'info']
                             	})
 ];
--- mt-web/src/main/webapp/app/wizard/wizard-steps/wizard-steps.component.js
+++ mt-web/src/main/webapp/app/wizard/wizard-steps/wizard-steps.component.js
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, forwardRef } from '@angular/core';
 
 import { WizardStepComponent } from 'app/wizard/wizard-steps/wizard-step.component';
 
 export class WizardStepsComponent {
 
 	constructor() {
+		this._current = null;
+	}
+
+	set current(current) {
+		this._current = current;
+		this.updateCurrentStep(current);
+	}
+
+	get current() {
+		return this._current;
+	}
+
+	updateCurrentStep(stepName) {
+		if (this.stepComponents) {
+			this.stepComponents.forEach(function(cmp){
+				cmp.isCurrent = cmp.name && stepName && cmp.name === stepName;
+				cmp.isVisited = cmp.isVisited || cmp.isCurrent;
+			});
+		}
+	}
+
+	ngAfterContentInit() {
+		//debugger
+		this.updateCurrentStep(this._current);
 	}
 
 }
@@ -14,6 +38,8 @@ WizardStepsComponent.annotations = [
                             		selector: 'wizard-steps',
                             		templateUrl: 'app/wizard/wizard-steps/wizard-steps.template.html',
                             		styleUrls:  ['app/wizard/wizard-steps/wizard-steps.component.css'],
-                            		directives: [WizardStepComponent]
+                            		directives: [WizardStepComponent],
+                            		queries: {stepComponents: new ContentChildren(forwardRef(() => WizardStepComponent))},
+                            		inputs: ['current']
                             	})
 ];
