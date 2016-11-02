82ca797e29b770fde199e506328034860f9b947b
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2016-11-02 16:51:33 +0200

    Introduce Bean Validation to business layer and repository layer

---
+++ mt-business-api/src/main/java/org/mt/business/api/domain/RegExp.java
@@ -0,0 +1,7 @@
+package org.mt.business.api.domain;
+
+public interface RegExp {
+
+    String EMAIL = "^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$";
+
+}
