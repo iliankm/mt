2360258e64c0e78a88914edb005899a107aaf928
iliankm <ilian.kamilov@gmail.com>
2016-12-06 17:00:56 +0200

    upload employee image rest resource

--- mt-web/src/main/java/org/mt/web/resource/RestEndpoints.java
+++ mt-web/src/main/java/org/mt/web/resource/RestEndpoints.java
@@ -14,4 +14,6 @@ public interface RestEndpoints {
 
     String EMPLOYEE_PHONES = "{id}/phones";
 
+    String UPLOAD = "{id}/upload";
+
 }
---
+++ mt-web/src/main/java/org/mt/web/resource/employees/UploadImageResource.java
@@ -0,0 +1,92 @@
+package org.mt.web.resource.employees;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.mt.web.resource.RestEndpoints;
+
+@Path(RestEndpoints.EMPLOYEES)
+public class UploadImageResource {
+
+    private static final int MAX_SIZE = 10 * 1024 * 1024;
+
+    private static final List<String> ALLOWED_FILE_TYPES = Arrays.asList("jpeg", "jpg", "png", "bmp", "tiff", "gif");
+
+    @POST
+    @Path(RestEndpoints.UPLOAD)
+    @Consumes("multipart/form-data")
+    public Response upload(@PathParam("id") String id, MultipartFormDataInput input) {
+
+	final Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+
+	final List<InputPart> inputParts = uploadForm.get("upload_file");
+
+	inputParts.forEach(inputPart -> {
+
+	    //get file name
+	    final String fileName = getFileName(inputPart.getHeaders());
+	    final String ext = fileName.toLowerCase().substring(fileName.lastIndexOf('.') + 1);
+
+	    // check for file type
+	    if (!ALLOWED_FILE_TYPES.contains(ext)) {
+		throw new RuntimeException("Unsupported filetype: " + ext);
+	    }
+
+	    try {
+		final InputStream inputStream = inputPart.getBody(InputStream.class, null);
+
+		final byte[] bytes = IOUtils.toByteArray(inputStream);
+
+		//check for max allowed file size
+		if (bytes.length > MAX_SIZE) {
+		    throw new RuntimeException(
+			    "Uploaded file size of " + bytes.length + " excedes max allowed " + MAX_SIZE);
+		}
+	    } catch (IOException e) {
+		throw new RuntimeException(e);
+	    }
+	});
+
+	return Response.status(Status.OK).type(MediaType.APPLICATION_JSON_TYPE).entity("OK").build();
+    }
+
+    /**
+     * Extract file name from part headers
+     * @param header
+     * @return file name or unknown
+     */
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+	final String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+	for (String filename : contentDisposition) {
+
+	    if (filename.trim().startsWith("filename")) {
+
+		final String[] name = filename.split("=");
+
+		final String finalFileName = name[1].trim().replaceAll("\"", "");
+
+		return finalFileName;
+	    }
+	}
+
+	return "unknown";
+    }
+
+}
