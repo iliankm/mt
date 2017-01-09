0a2db5a9781fed060f0e94b8d356984158c90377
ilian.kamilov@gmail.com <ilian.kamilov@gmail.com>
2017-01-09 20:04:29 +0200

    Implementation of ImagesService

---
+++ mt-business-api/src/main/java/org/mt/business/api/boundary/service/employee/ImagesService.java
@@ -0,0 +1,20 @@
+package org.mt.business.api.boundary.service.employee;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+
+public interface ImagesService {
+
+    /**
+     * Save given input stream to file in the application upload folder.
+     *
+     * Directory structure is ensured and missing folders are created if not exist.
+     *
+     * @param employeeId - Employee id
+     * @param inputStream - image input stream
+     * @param fileName - original filename
+     * @return - Path of the saved file
+     */
+    Path save(String employeeId, InputStream in, String fileName);
+
+}
---
+++ mt-business/src/main/java/org/mt/business/boundary/service/employee/ImagesServiceBean.java
@@ -0,0 +1,75 @@
+package org.mt.business.boundary.service.employee;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.mt.business.api.boundary.service.employee.ImagesService;
+
+public class ImagesServiceBean implements ImagesService {
+
+    private static final String ENV_HOME = "HOME";
+
+    private static final String APP_UPLOAD_DIR = "mt-upload";
+
+    @Override
+    public Path save(String employeeId, InputStream in, String fileName) {
+
+	Objects.requireNonNull(employeeId);
+
+	Objects.requireNonNull(in);
+
+	Objects.requireNonNull(fileName);
+
+	// get employee images dir - if there are some missing folders, they are
+	// created
+	final Path employeeImagesDirectory = getEmployeeImagesDirectory(employeeId);
+
+	// target file Path
+	final Path targetFilePath = Paths.get(employeeImagesDirectory.toString(),
+		UUID.randomUUID().toString().concat(fileName));
+
+	// copy InputStream to the target file
+	try {
+	    Files.copy(in, targetFilePath);
+	} catch (IOException e) {
+	    throw new RuntimeException(e);
+	}
+
+	return targetFilePath;
+    }
+
+    /**
+     *
+     * @param employeeId - Employee id
+     * @return - employee images directory Path. All nonexistent parent
+     *         directories are created.
+     */
+    private Path getEmployeeImagesDirectory(String employeeId) {
+
+	final String homePathString = System.getenv(ENV_HOME);
+
+	if (homePathString == null || homePathString.isEmpty()) {
+	    throw new RuntimeException("Missing environment variable HOME");
+	}
+
+	try {
+
+	    final Path path = Paths.get(homePathString, APP_UPLOAD_DIR, employeeId);
+
+	    if (!path.toFile().exists()) {
+		return Files.createDirectories(path);
+	    } else {
+		return path;
+	    }
+
+	} catch (IOException e) {
+	    throw new RuntimeException(e);
+	}
+    }
+
+}
